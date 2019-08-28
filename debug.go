d448c1e5063256d4ccfeae915a0f6e669d40594e
llllllllll <38289577+GitForZHHY@users.noreply.github.com>
2019-08-28 20:43:08 +0800

    Update debug.go

--- debug.go
+++ debug.go
@@ -13,81 +13,6 @@ import (
 )
 
 /*
- * Func_name :DealWithLogInfo
- * Func      :
- * Author    :zhhy
- * Date      :20190317
- *
- */
-func DealWithLogInfo(loginfo string,args []interface{}) (outinfo string) {
-
-	var args_buf strings.Builder
-	var loginfo_buf strings.Builder
-	//var loginfo_tmp_str string
-	var ArgsNum int
-
-	Max_Len := len(loginfo)
-
-
-	fmt.Printf("loginfolen[%d]\n",Max_Len)
-	for i := 0; i < Max_Len; {
-		lasti := i
-		for i < Max_Len && loginfo[i] != '%' {
-			i++
-		}     
-
-		if i > lasti {
-			//loginfo_buf.Write([]byte(loginfo[lasti:i]))
-			loginfo_buf.WriteString(loginfo[lasti:i])
-			loginfo_tmp_str := args_buf.String()
-			fmt.Printf("lasg[%d]i[%d]loginfonow- %s \n",lasti,i,loginfo_tmp_str)
-			fmt.Printf("lasg[%d]i[%d]loginfonow- %s \n",lasti,i,loginfo[lasti:i])
-		}     
-		if i >= Max_Len {
-			// done processing format string
-			//args_buf.Write([]byte(loginfo[lasti:i]))
-			args_buf.WriteString(loginfo[lasti:i])
-			outinfo = args_buf.String()
-
-			fmt.Printf("DEAL DONE!!!\n")
-			break
-		}     
-
-		// Process one verb
-		args_buf.Write([]byte(loginfo[lasti:i]))
-
-		i++
-		//for _,arg := range args {
-		arg := args[ArgsNum]
-		switch verb := arg.(type) {
-		case string:
-			args_buf.WriteString(verb)
-			outinfo = args_buf.String()
-		case int:
-			int_str_tmp := strconv.Itoa(verb)
-			args_buf.WriteString(int_str_tmp)
-			outinfo = args_buf.String()
-
-			case bool:      
-			case []byte:    
-			case float32:   
-			case int8:      
-			case float64:   
-			case int16:     
-			case int32:     
-			case int64:     
-			log.Fatal("u need wait...")
-		default:
-			log.Fatal("unknown type")
-		}
-		//	}
-		ArgsNum++
-		i++   
-	}
-
-	 return outinfo
-}
-/*
  * Func_name :swVdebug
  * Func      :
  * Author    :zhhy
@@ -97,11 +22,9 @@ func DealWithLogInfo(loginfo string,args []interface{}) (outinfo string) {
 
 func swVdebug(msg string,args ...interface{}){
  
-	msg_str := DealWithLogInfo(msg,args)
-	fmt.Printf("LOGMSG: %s\n",msg_str)
 	var  line int
 	//org_file_name,line := "???",0
-
+        
 	_,org_file_name,_,ok := runtime.Caller(0)
 	if ok {
 		//funcname = runtime.FuncForPC(pc).Name()
@@ -116,7 +39,8 @@ func swVdebug(msg string,args ...interface{}){
 	if err != nil {
 		log.Printf("open %s fail...\n",file_name)
 	}
-
+	msg_str := fmt.Sprintf(msg,args...)
+	
 	//time|pid|file_name|line|msg
 	//file_line := fmt.Sprintf("%s|%s|%s|%d|%s\n",time.Now().Format("2006/01/02-15:04:05"),org_file_name,funcname,line,msg)
 	file_line := fmt.Sprintf(" %s|%6d|%s|%5d|%s   \n",time.Now().Format("2006/01/02-15:04:05"),os.Getpid(),org_file_name,line,msg_str)
